d6b32a9cf1c1642a766d44f0e98963128ebb5508
Aleksandra Maneva <maneva.aleksandra@gmail.com>
2022-10-31 12:57:30 +0100

    Add job/:id endpoint

--- api-server/src/main.py
+++ api-server/src/main.py
@@ -1,14 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-@app.get("/")
-def health():
-    return {"health": "OK"}
-
-@app.get("/job")
-def getJobs():
-    return[{
+jobs_data = [{
         "id": "css_123",
         "title": "Web Developer",
         "description": "A web developer make and maintain websites." +
@@ -22,3 +16,17 @@ def getJobs():
         " also known as uptime."
     }]
 
+@app.get("/")
+def health():
+    return {"health": "OK"}
+
+@app.get("/job")
+def getJobs():
+    return jobs_data
+
+@app.get("/job/{id}")
+def getJob(id):
+    for job in jobs_data:
+        if (job["id"] == id): 
+            return job
+    raise HTTPException (status_code=404, detail="Job with id" + id + "not found")
